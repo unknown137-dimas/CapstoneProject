0deab673bce7aa009ed88da5d8b6c38ce97994fd
Vincentius Willy Ardiyanto <72459873+WillyArdiyanto@users.noreply.github.com>
2022-06-12 22:48:16 +0700

    Update auth.js
    Response update

--- CC/routes/auth.js
+++ CC/routes/auth.js
@@ -26,7 +26,8 @@ router.post("/register", async (req, res) => {
   });
   try {
     const savedUser = await user.save();
-    res.send({ user: user._id });
+    res.send({ "error": false,
+    "message": "User Created" });
   } catch (err) {
     res.status(400).send(err);
   }
